a6a2d0f30dcd8ffb9c0b94706f9201ba2a238c5f
Wulfy <2012135+wulfygrl@users.noreply.github.com>
2025-08-08 18:25:17 -0400

    Create wulfyTools.lua

---
+++ wulfyTools.lua
@@ -0,0 +1,275 @@
+-- Copy the code between UNIVERSAL FUNCTIONS and END UNIVERSAL to the top of 
+-- any object in order to use this module's utility funcitons. 
+
+--[[ UNIVERSAL FUNCTIONS ]]--
+moduleVersion = 0.01
+pID = 'w_utils'
+LOG_STYLES = {}
+MOD_DATA = {
+  modVersion = moduleVersion,
+  modID = pID,
+  modName = 'Wulfy Utils',
+  gitFileName = 'wulfyUtils.lua',
+  colors = {
+    init = {
+      primary = 'h0',
+      secondary = 'h210'
+    }
+  },
+}
+ENC_DATA = {
+  pID=pID
+}
+-- retrieve the utils module
+function utils() return Global.getVar('wulfy_utils') end
+-- Log message wrapper for this module.
+function wLog(msg, pre, tags)
+  utils().call('log_wrapper', {
+    pID = MOD_DATA.modID,
+    color = MOD_DATA.colors.primary.mlight,
+    msg = msg,
+    pre = pre,
+    tags = tags,
+  })
+end
+-- register this module
+function registerModule() utils().call('registerWulfyMod', ENC_DATA) end
+-- unregister this module
+function unRegisterModule() utils().call('unregisterWulfyMod', ENC_DATA) end
+-- update this module
+function selfUpdate() utils().call('updateCheck', MOD_DATA) end
+-- initialize colors
+function color() 
+  if MOD_DATA.colors.init ~= nil then
+    MOD_DATA.colors = utils().call('getColors', MOD_DATA.colors.init)
+  end
+  return MOD_DATA.colors
+end
+--[[ END UNIVERSAL ]]--
+
+--[[ UTILITY FUNCTIONS ]]--
+
+--[[ Wrapper to color log messages according to the module's palette.
+params:
+  pID: module's primary property ID
+  color: color for log messages with this label
+  msg, pre, tags: regular parameters to the TTS log() function
+--]]
+function log_wrapper(p)
+  local color = p.color or MOD_DATA.colors.primary.mlight
+  local pID = p.pID or pID
+  if LOG_STYLES[pID] ~= color then
+    logStyle(pID, color, '', '')
+    LOG_STYLES[pID] = color
+  end
+  local msg = p.msg or ''
+  local pre = p.pre or ''
+  local tags = p.tags or pID
+  log(msg, pre, tags)
+end
+
+function registerWulfyMod(enc_data)
+  local props = enc_data.properties or {}
+  local vals = enc_data.values or {}
+  local menus = enc_data.menus or {}
+  local enc = Global.getVar('Encoder')
+  if enc == nil then wLog('No encoder found') return end
+  for _, prop in ipairs(ENCDATA.properties) do repeat
+    if enc.call('APIpropertyExists', prop) then break end
+    enc.call('APIregisterProperty', prop)
+  until true end
+  for _, val in ipairs(ENCDATA.values) do repeat
+    if enc.call('APIvalueExists', val) then break end
+    enc.call('APIregisterValue', val)
+  until true end
+  for _, menu in ipairs(ENCDATA.menus) do
+    enc.call('APIregisterMenu', menu)
+  end
+end
+
+function unregisterWulfyMod(enc_data) end
+function updateCheck(mod_data) end
+function getColors(p)
+  local c1,c2
+  local hues = utils().HUE_PALETTES
+  if p.primary ~= nil then c1 = hues[p.primary] end
+  if p.secondary ~= nil then c2 = hues[p.secondary] end
+  return {
+    primary = c1 or HUE_PALETTES.h330,
+    secondary = c2 or HUE_PALETTES.h225,
+  }
+end
+
+function onLoad() Global.setVar('wulfy_utils', self) end
+function pass() return nil end
+
+HUE_PALETTES = {
+  h0 = { -- Red
+    light = { 254 / 255, 169 / 255, 169 / 255 },
+    mlight = { 211 / 255, 105 / 255, 105 / 255 },
+    mid = { 169 / 255, 56 / 255, 56 / 255 },
+    mdark = { 127 / 255, 21 / 255, 21 / 255 },
+    dark = { 85 / 255, 0 / 255, 0 / 255 },
+  },
+  h15 = {
+    light = { 254 / 255, 193 / 255, 169 / 255 },
+    mlight = { 211 / 255, 135 / 255, 105 / 255 },
+    mid = { 169 / 255, 89 / 255, 56 / 255 },
+    mdark = { 127 / 255, 51 / 255, 21 / 255 },
+    dark = { 85 / 255, 24 / 255, 0 / 255 },
+  },
+  h30 = { -- Red-orange
+    light = { 254 / 255, 207 / 255, 169 / 255 },
+    mlight = { 211 / 255, 153 / 255, 105 / 255 },
+    mid = { 169 / 255, 108 / 255, 56 / 255 },
+    mdark = { 127 / 255, 69 / 255, 21 / 255 },
+    dark = { 85 / 255, 38 / 255, 0 / 255 },
+  },
+  h45 = {
+    light = { 254 / 255, 218 / 255, 169 / 255 },
+    mlight = { 211 / 255, 166 / 255, 105 / 255 },
+    mid = { 169 / 255, 121 / 255, 56 / 255 },
+    mdark = { 127 / 255, 82 / 255, 21 / 255 },
+    dark = { 85 / 255, 48 / 255, 0 / 255 },
+  },
+  h60 = { -- Orange
+    light = { 254 / 255, 226 / 255, 169 / 255 },
+    mlight = { 211 / 255, 176 / 255, 105 / 255 },
+    mid = { 169 / 255, 132 / 255, 56 / 255 },
+    mdark = { 127 / 255, 92 / 255, 21 / 255 },
+    dark = { 85 / 255, 56 / 255, 0 / 255 },
+  },
+  h75 = {
+    light = { 254 / 255, 233 / 255, 169 / 255 },
+    mlight = { 211 / 255, 185 / 255, 105 / 255 },
+    mid = { 169 / 255, 141 / 255, 56 / 255 },
+    mdark = { 127 / 255, 101 / 255, 21 / 255 },
+    dark = { 85 / 255, 63 / 255, 0 / 255 },
+  },
+  h90 = { -- Yellow-orange
+    light = { 254 / 255, 239 / 255, 169 / 255 },
+    mlight = { 211 / 255, 193 / 255, 105 / 255 },
+    mid = { 169 / 255, 150 / 255, 56 / 255 },
+    mdark = { 127 / 255, 109 / 255, 21 / 255 },
+    dark = { 85 / 255, 70 / 255, 0 / 255 },
+  },
+  h105 = {
+    light = { 254 / 255, 246 / 255, 169 / 255 },
+    mlight = { 211 / 255, 202 / 255, 105 / 255 },
+    mid = { 169 / 255, 159 / 255, 56 / 255 },
+    mdark = { 127 / 255, 118 / 255, 21 / 255 },
+    dark = { 85 / 255, 77 / 255, 0 / 255 },
+  },
+  h120 = { -- Yellow
+    light = { 254 / 255, 254 / 255, 169 / 255 },
+    mlight = { 211 / 255, 211 / 255, 105 / 255 },
+    mid = { 169 / 255, 169 / 255, 56 / 255 },
+    mdark = { 127 / 255, 127 / 255, 21 / 255 },
+    dark = { 85 / 255, 85 / 255, 0 / 255 },
+  },
+  h135 = {
+    light = { 231 / 255, 245 / 255, 163 / 255 },
+    mlight = { 186 / 255, 204 / 255, 102 / 255 },
+    mid = { 145 / 255, 164 / 255, 55 / 255 },
+    mdark = { 105 / 255, 123 / 255, 21 / 255 },
+    dark = { 68 / 255, 82 / 255, 0 / 255 },
+  },
+  h150 = { -- Light Green
+    light = { 211 / 255, 237 / 255, 158 / 255 },
+    mlight = { 164 / 255, 197 / 255, 98 / 255 },
+    mid = { 123 / 255, 158 / 255, 53 / 255 },
+    mdark = { 86 / 255, 119 / 255, 20 / 255 },
+    dark = { 53 / 255, 79 / 255, 0 / 255 },
+  },
+  h165 = {
+    light = { 185 / 255, 226 / 255, 150 / 255 },
+    mlight = { 136 / 255, 188 / 255, 94 / 255 },
+    mid = { 96 / 255, 151 / 255, 50 / 255 },
+    mdark = { 62 / 255, 113 / 255, 19 / 255 },
+    dark = { 34 / 255, 75 / 255, 0 / 255 },
+  },
+  h180 = { -- Green
+    light = { 135 / 255, 203 / 255, 135 / 255 },
+    mlight = { 84 / 255, 169 / 255, 84 / 255 },
+    mid = { 45 / 255, 135 / 255, 45 / 255 },
+    mdark = { 17 / 255, 102 / 255, 17 / 255 },
+    dark = { 0 / 255, 68 / 255, 0 / 255 },
+  },
+  h195 = {
+    light = { 116 / 255, 174 / 255, 149 / 255 },
+    mlight = { 72 / 255, 145 / 255, 114 / 255 },
+    mid = { 39 / 255, 116 / 255, 83 / 255 },
+    mdark = { 15 / 255, 87 / 255, 56 / 255 },
+    dark = { 0 / 255, 58 / 255, 33 / 255 },
+  },
+  h210 = { -- Teal
+    light = { 101 / 255, 152 / 255, 152 / 255 },
+    mlight = { 63 / 255, 127 / 255, 127 / 255 },
+    mid = { 34 / 255, 102 / 255, 102 / 255 },
+    mdark = { 13 / 255, 76 / 255, 76 / 255 },
+    dark = { 0 / 255, 51 / 255, 51 / 255 },
+  },
+  h225 = {
+    light = { 112 / 255, 141 / 255, 163 / 255 },
+    mlight = { 72 / 255, 108 / 255, 136 / 255 },
+    mid = { 41 / 255, 79 / 255, 109 / 255 },
+    mdark = { 18 / 255, 54 / 255, 82 / 255 },
+    dark = { 4 / 255, 32 / 255, 54 / 255 },
+  },
+  h240 = { -- Blue
+    light = { 119 / 255, 135 / 255, 170 / 255 },
+    mlight = { 78 / 255, 97 / 255, 141 / 255 },
+    mid = { 46 / 255, 66 / 255, 113 / 255 },
+    mdark = { 22 / 255, 41 / 255, 85 / 255 },
+    dark = { 6 / 255, 21 / 255, 57 / 255 },
+  },
+  h255 = {
+    light = { 127 / 255, 127 / 255, 178 / 255 },
+    mlight = { 85 / 255, 85 / 255, 148 / 255 },
+    mid = { 51 / 255, 51 / 255, 119 / 255 },
+    mdark = { 26 / 255, 26 / 255, 89 / 255 },
+    dark = { 9 / 255, 9 / 255, 59 / 255 },
+  },
+  h270 = { -- Indigo
+    light = { 135 / 255, 123 / 255, 175 / 255 },
+    mlight = { 96 / 255, 81 / 255, 145 / 255 },
+    mid = { 64 / 255, 48 / 255, 117 / 255 },
+    mdark = { 38 / 255, 23 / 255, 87 / 255 },
+    dark = { 19 / 255, 7 / 255, 58 / 255 },
+  },
+  h285 = {
+    light = { 142 / 255, 120 / 255, 172 / 255 },
+    mlight = { 106 / 255, 78 / 255, 143 / 255 },
+    mid = { 75 / 255, 45 / 255, 115 / 255 },
+    mdark = { 49 / 255, 21 / 255, 86 / 255 },
+    dark = { 27 / 255, 5 / 255, 57 / 255 },
+  },
+  h300 = { -- Purple
+    light = { 150 / 255, 116 / 255, 170 / 255 },
+    mlight = { 117 / 255, 74 / 255, 141 / 255 },
+    mid = { 88 / 255, 42 / 255, 113 / 255 },
+    mdark = { 61 / 255, 18 / 255, 85 / 255 },
+    dark = { 37 / 255, 3 / 255, 56 / 255 },
+  },
+  h315 = {
+    light = { 165 / 255, 110 / 255, 165 / 255 },
+    mlight = { 137 / 255, 68 / 255, 137 / 255 },
+    mid = { 110 / 255, 37 / 255, 110 / 255 },
+    mdark = { 83 / 255, 14 / 255, 83 / 255 },
+    dark = { 55 / 255, 0 / 255, 55 / 255 },
+  },
+  h330 = { -- Pink
+    light = { 204 / 255, 136 / 255, 174 / 255 },
+    mlight = { 169 / 255, 84 / 255, 132 / 255 },
+    mid = { 136 / 255, 45 / 255, 96 / 255 },
+    mdark = { 102 / 255, 17 / 255, 65 / 255 },
+    dark = { 68 / 255, 0 / 255, 38 / 255 },
+  },
+  h345 = {
+    light = { 227 / 255, 151 / 255, 174 / 255 },
+    mlight = { 189 / 255, 94 / 255, 123 / 255 },
+    mid = { 151 / 255, 50 / 255, 81 / 255 },
+    mdark = { 114 / 255, 19 / 255, 48 / 255 },
+    dark = { 76 / 255, 0 / 255, 23 / 255 },
+  },
+}
